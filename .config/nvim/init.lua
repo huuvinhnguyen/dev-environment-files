5edd3142d9647cd04f9f144baf7ac8382acb2b5b
Vinh Nguyen (Bin) <huuvinh@live.com>
2023-02-09 13:18:32 +0700

    Add files via upload

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,9 @@ require("vinh.plugins-setup")
 require("vinh.core.options")
 require("vinh.core.keymaps")
 require("vinh.core.colorscheme")
+require("vinh.plugins.comment")
+require("vinh.plugins.nvim-tree")
+require("vinh.plugins.lualine")
+require("vinh.plugins.telescope")
+
+
