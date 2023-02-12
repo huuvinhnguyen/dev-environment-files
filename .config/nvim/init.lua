42d0016e5df90a0f2cca689e5927de5e98994977
Vinh Nguyen (Bin) <huuvinh@live.com>
2023-02-12 13:57:37 +0700

    Add files via upload

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,5 +6,12 @@ require("vinh.plugins.comment")
 require("vinh.plugins.nvim-tree")
 require("vinh.plugins.lualine")
 require("vinh.plugins.telescope")
-
+require("vinh.plugins.nvim-cmp")
+require("vinh.plugins.lsp.mason")
+require("vinh.plugins.lsp.lspsaga")
+require("vinh.plugins.lsp.lspconfig")
+require("vinh.plugins.lsp.null-ls")
+require("vinh.plugins.autopairs")
+require("vinh.plugins.treesitter")
+require("vinh.plugins.gitsigns")
 
